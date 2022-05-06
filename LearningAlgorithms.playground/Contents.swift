a31648a3ba4d0aa2d638644f6191b84d9c390e90
HieuTong <hieuminhtongle20111998@gmail.com>
2022-05-06 23:09:23 +0700

    AVL Tree

--- LearningAlgorithms.playground/Contents.swift
+++ LearningAlgorithms.playground/Contents.swift
@@ -1,5 +1,4 @@
 import UIKit
-
 public func example(of description: String, action: () -> ()) {
   print("---Example of \(description)---")
   action()
@@ -594,66 +593,265 @@ public func example(of description: String, action: () -> ()) {
 //  tree.forEachLevelOrder { print($0.value) }
 //}
 
-// Binary tree
+//// Binary tree
+//
+//public class BinaryNode<Element> {
+//
+//    public var value: Element
+//    public var leftChild: BinaryNode?
+//    public var rightChild: BinaryNode?
+//
+//    public init(value: Element) {
+//        self.value = value
+//    }
+//}
+//
+//var tree: BinaryNode<Int> = {
+//    let zero = BinaryNode(value: 0)
+//    let one = BinaryNode(value: 1)
+//    let five = BinaryNode(value: 5)
+//    let seven = BinaryNode(value: 7)
+//    let eight = BinaryNode(value: 8)
+//    let nine = BinaryNode(value: 9)
+//
+//    seven.leftChild = one
+//    one.leftChild = zero
+//    one.rightChild = five
+//    seven.rightChild = nine
+//    nine.leftChild = eight
+//
+//    return seven
+//}()
+//
+//extension BinaryNode: CustomStringConvertible {
+//
+//  public var description: String {
+//    diagram(for: self)
+//  }
+//
+//  private func diagram(for node: BinaryNode?,
+//                       _ top: String = "",
+//                       _ root: String = "",
+//                       _ bottom: String = "") -> String {
+//    guard let node = node else {
+//      return root + "nil\n"
+//    }
+//    if node.leftChild == nil && node.rightChild == nil {
+//      return root + "\(node.value)\n"
+//    }
+//    return diagram(for: node.rightChild,
+//                   top + " ", top + "┌──", top + "│ ")
+//         + root + "\(node.value)\n"
+//         + diagram(for: node.leftChild,
+//                   bottom + "│ ", bottom + "└──", bottom + " ")
+//  }
+//}
+//
+//
+////example(of: "tree diagram") {
+////    print(tree)
+////}
+//
+//extension BinaryNode {
+//    public func traverseInOrder(visit: (Element) -> Void) {
+//        leftChild?.traverseInOrder(visit: visit)
+//        visit(value)
+//        rightChild?.traverseInOrder(visit: visit)
+//    }
+//
+//    public func traversePreOrder(visit: (Element) -> Void) {
+//        visit(value)
+//        leftChild?.traversePreOrder(visit: visit)
+//        rightChild?.traversePreOrder(visit: visit)
+//    }
+//
+//    public func traversePostOrder(visit: (Element) -> Void) {
+//        leftChild?.traversePostOrder(visit: visit)
+//        rightChild?.traversePostOrder(visit: visit)
+//        visit(value)
+//    }
+//}
+//
+//public struct BinarySearchTree<Element: Comparable> {
+//
+//    public private(set) var root: BinaryNode<Element>?
+//
+//    public init() {}
+//}
+//
+//extension BinarySearchTree: CustomStringConvertible {
+//
+//    public var description: String {
+//        guard let root = root else { return "Empty tree" }
+//        return String(describing: root)
+//    }
+//}
+//
+//extension BinarySearchTree {
+//
+//    public mutating func insert(_ value: Element) {
+//        root = insert(from: root, value: value)
+//    }
+//
+//    private func insert(from node: BinaryNode<Element>?, value: Element) -> BinaryNode<Element> {
+//        // 1
+//        guard let node = node else {
+//            return BinaryNode(value: value)
+//        }
+//
+//        if value < node.value {
+//            node.leftChild = insert(from: node.leftChild, value: value)
+//        } else {
+//            node.rightChild = insert(from: node.rightChild, value: value)
+//        }
+//
+//        return node
+//    }
+//
+//    public func contains(_ value: Element) -> Bool {
+//        var current = root
+//
+//        while let node = current {
+//            if node.value == value {
+//                return true
+//            }
+//
+//            if value < node.value {
+//                current = node.leftChild
+//            } else {
+//                current = node.rightChild
+//            }
+//        }
+//
+//        return false
+//    }
+//}
+//
+//private extension BinaryNode {
+//
+//    var min: BinaryNode {
+//        leftChild?.min ?? self
+//    }
+//}
+//
+//extension BinarySearchTree {
+//
+//    public mutating func remove(_ value: Element) {
+//        root = remove(node: root, value: value)
+//    }
+//
+//    private func remove(node: BinaryNode<Element>?, value: Element) -> BinaryNode<Element>? {
+//        guard let node = node else {
+//            return nil
+//        }
+//
+//        if value == node.value {
+//            if node.leftChild == nil && node.rightChild == nil {
+//                return nil
+//            }
+//
+//            if node.leftChild == nil {
+//                return node.rightChild
+//            }
+//
+//            if node.rightChild == nil {
+//                return node.leftChild
+//            }
+//
+//            node.value = node.rightChild!.min.value
+//            node.rightChild = remove(node: node.rightChild, value: node.value)
+//
+//        } else if value < node.value {
+//            node.leftChild = remove(node: node.leftChild, value: value)
+//        } else {
+//            node.rightChild = remove(node: node.rightChild, value: value)
+//        }
+//
+//        return node
+//    }
+//}
+//
+//var exampleTree: BinarySearchTree<Int> {
+//  var bst = BinarySearchTree<Int>()
+//  bst.insert(3)
+//  bst.insert(1)
+//  bst.insert(4)
+//  bst.insert(0)
+//  bst.insert(2)
+//  bst.insert(5)
+//  return bst
+//}
+//
+//example(of: "building a BST") {
+////  var bst = BinarySearchTree<Int>()
+////  for i in 0..<5 {
+////    bst.insert(i)
+////  }
+////  print(bst)
+//
+////    print(exampleTree)
+//}
+//
+//example(of: "removing a node") {
+//    var tree = exampleTree
+//    print("Tree before removal: ")
+//    print(tree)
+//    tree.remove(3)
+//    print("Tree after remove root: ")
+//    print(tree)
+//}
 
-public class BinaryNode<Element> {
+//AVL Tree
+
+public class AVLNode<Element> {
     
     public var value: Element
-    public var leftChild: BinaryNode?
-    public var rightChild: BinaryNode?
+    public var leftChild: AVLNode?
+    public var rightChild: AVLNode?
     
     public init(value: Element) {
         self.value = value
     }
-}
-
-var tree: BinaryNode<Int> = {
-    let zero = BinaryNode(value: 0)
-    let one = BinaryNode(value: 1)
-    let five = BinaryNode(value: 5)
-    let seven = BinaryNode(value: 7)
-    let eight = BinaryNode(value: 8)
-    let nine = BinaryNode(value: 9)
-
-    seven.leftChild = one
-    one.leftChild = zero
-    one.rightChild = five
-    seven.rightChild = nine
-    nine.leftChild = eight
     
-    return seven
-}()
-
-extension BinaryNode: CustomStringConvertible {
-
-  public var description: String {
-    diagram(for: self)
-  }
-
-  private func diagram(for node: BinaryNode?,
-                       _ top: String = "",
-                       _ root: String = "",
-                       _ bottom: String = "") -> String {
-    guard let node = node else {
-      return root + "nil\n"
+    public var height = 0
+    
+    public var balanceFactor: Int {
+        leftHeight - rightHeight
     }
-    if node.leftChild == nil && node.rightChild == nil {
-      return root + "\(node.value)\n"
+    
+    public var leftHeight: Int {
+        leftChild?.height ?? -1
     }
-    return diagram(for: node.rightChild,
-                   top + " ", top + "┌──", top + "│ ")
-         + root + "\(node.value)\n"
-         + diagram(for: node.leftChild,
-                   bottom + "│ ", bottom + "└──", bottom + " ")
-  }
+    
+    public var rightHeight: Int {
+        rightChild?.height ?? -1
+    }
 }
 
+extension AVLNode: CustomStringConvertible {
+    
+    public var description: String {
+       diagram(for: self)
+    }
+    
+    private func diagram(for node: AVLNode?,
+                           _ top: String = "",
+                           _ root: String = "",
+                           _ bottom: String = "") -> String {
+        guard let node = node else {
+          return root + "nil\n"
+        }
+        if node.leftChild == nil && node.rightChild == nil {
+          return root + "\(node.value)\n"
+        }
+        return diagram(for: node.rightChild, top + " ", top + "┌──", top + "│ ")
+          + root + "\(node.value)\n"
+          + diagram(for: node.leftChild, bottom + "│ ", bottom + "└──", bottom + " ")
+    }
+}
 
-//example(of: "tree diagram") {
-//    print(tree)
-//}
-
-extension BinaryNode {
+extension AVLNode {
+    
     public func traverseInOrder(visit: (Element) -> Void) {
         leftChild?.traverseInOrder(visit: visit)
         visit(value)
@@ -673,50 +871,29 @@ extension BinaryNode {
     }
 }
 
-public struct BinarySearchTree<Element: Comparable> {
+public struct AVLTree<Element: Comparable> {
     
-    public private(set) var root: BinaryNode<Element>?
-    
+    public private(set) var root: AVLNode<Element>?
+
     public init() {}
 }
 
-extension BinarySearchTree: CustomStringConvertible {
+extension AVLTree: CustomStringConvertible {
     
     public var description: String {
-        guard let root = root else { return "Empty tree" }
+        guard let root = root else { return "Empty tree "}
         return String(describing: root)
     }
 }
 
-extension BinarySearchTree {
+extension AVLTree {
     
-    public mutating func insert(_ value: Element) {
-        root = insert(from: root, value: value)
-    }
-    
-    private func insert(from node: BinaryNode<Element>?, value: Element) -> BinaryNode<Element> {
-        // 1
-        guard let node = node else {
-            return BinaryNode(value: value)
-        }
-        
-        if value < node.value {
-            node.leftChild = insert(from: node.leftChild, value: value)
-        } else {
-            node.rightChild = insert(from: node.rightChild, value: value)
-        }
-        
-        return node
-    }
-
-    public func contains(_ value: Element) -> Bool {
+    public func constants(_ value: Element) -> Bool {
         var current = root
-        
         while let node = current {
             if node.value == value {
                 return true
             }
-            
             if value < node.value {
                 current = node.leftChild
             } else {
@@ -728,20 +905,99 @@ extension BinarySearchTree {
     }
 }
 
-private extension BinaryNode {
+private extension AVLNode {
     
-    var min: BinaryNode {
+    var min: AVLNode {
         leftChild?.min ?? self
     }
 }
 
-extension BinarySearchTree {
+extension AVLTree {
+    
+    private func leftRotate(_ node: AVLNode<Element>) -> AVLNode<Element> {
+        
+        let pivot = node.rightChild!
+        node.rightChild = pivot.leftChild
+        
+        pivot.leftChild = node
+        
+        node.height = max(node.leftHeight, node.rightHeight) + 1
+        pivot.height = max(pivot.leftHeight, pivot.rightHeight) + 1
+        return pivot
+    }
+    
+    private func rightRotate(_ node: AVLNode<Element>) -> AVLNode<Element> {
+        
+        let pivot = node.leftChild!
+        node.rightChild = pivot.leftChild
+        pivot.rightChild = node
+        
+        node.height = max(node.leftHeight, node.rightHeight) + 1
+        pivot.height = max(pivot.leftHeight, pivot.rightHeight) + 1
+        return pivot
+    }
+    
+    private func rightLeftRotate(_ node: AVLNode<Element>) -> AVLNode<Element> {
+        guard let rightChild = node.rightChild else {
+            return node
+        }
+        node.rightChild = rightRotate(rightChild)
+        return leftRotate(node)
+    }
+    
+    private func leftRightRotate(_ node: AVLNode<Element>) -> AVLNode<Element> {
+        guard let leftChild = node.leftChild else {
+            return node
+        }
+        node.leftChild = leftRotate(leftChild)
+        return rightRotate(node)
+    }
+    
+    private func balanced(_ node : AVLNode<Element>) -> AVLNode<Element> {
+        switch node.balanceFactor {
+        case 2:
+            if let leftChild = node.leftChild, leftChild.balanceFactor == -1 {
+                return leftRightRotate(node)
+            } else {
+                return rightRotate(node)
+            }
+        case -2:
+            if let rightChild = node.rightChild, rightChild.balanceFactor == 1 {
+                return rightLeftRotate(node)
+            } else {
+                return leftRotate(node)
+            }
+        default:
+          return node
+        }
+    }
+    
+    public mutating func insert(_ value: Element) {
+        root = insert(from: root, value: value)
+    }
+    
+    private func insert(from node: AVLNode<Element>?,
+                        value: Element) -> AVLNode<Element> {
+        guard let node = node else {
+            return AVLNode(value: value)
+        }
+        
+        if value < node.value {
+            node.leftChild = insert(from: node.leftChild, value: value)
+        } else {
+            node.rightChild = insert(from: node.rightChild, value: value)
+        }
+        
+        let balancedNode = balanced(node)
+        balancedNode.height = max(balancedNode.leftHeight, balancedNode.rightHeight) + 1
+        return balancedNode
+    }
     
     public mutating func remove(_ value: Element) {
         root = remove(node: root, value: value)
     }
     
-    private func remove(node: BinaryNode<Element>?, value: Element) -> BinaryNode<Element>? {
+    private func remove(node: AVLNode<Element>?, value: Element) -> AVLNode<Element>? {
         guard let node = node else {
             return nil
         }
@@ -750,59 +1006,44 @@ extension BinarySearchTree {
             if node.leftChild == nil && node.rightChild == nil {
                 return nil
             }
-            
             if node.leftChild == nil {
                 return node.rightChild
             }
-            
             if node.rightChild == nil {
                 return node.leftChild
             }
-            
             node.value = node.rightChild!.min.value
             node.rightChild = remove(node: node.rightChild, value: node.value)
-            
         } else if value < node.value {
             node.leftChild = remove(node: node.leftChild, value: value)
         } else {
             node.rightChild = remove(node: node.rightChild, value: value)
         }
         
-        return node
+        let balancedNode = balanced(node)
+        balancedNode.height = max(balancedNode.leftHeight, balancedNode.rightHeight) + 1
+        return balancedNode
     }
 }
 
-var exampleTree: BinarySearchTree<Int> {
-  var bst = BinarySearchTree<Int>()
-  bst.insert(3)
-  bst.insert(1)
-  bst.insert(4)
-  bst.insert(0)
-  bst.insert(2)
-  bst.insert(5)
-  return bst
-}
-
-example(of: "building a BST") {
-//  var bst = BinarySearchTree<Int>()
-//  for i in 0..<5 {
-//    bst.insert(i)
+//example(of: "repeated insertions in sequence") {
+//  var tree = AVLTree<Int>()
+//  for i in 0..<15 {
+//    tree.insert(i)
 //  }
-//  print(bst)
-    
-//    print(exampleTree)
+//  print(tree)
+//}
+
+example(of: "removing a value") {
+  var tree = AVLTree<Int>()
+  tree.insert(15)
+  tree.insert(10)
+  tree.insert(16)
+  tree.insert(18)
+  print(tree)
+  tree.remove(10)
+  print(tree)
 }
-
-example(of: "removing a node") {
-    var tree = exampleTree
-    print("Tree before removal: ")
-    print(tree)
-    tree.remove(3)
-    print("Tree after remove root: ")
-    print(tree)
-}
-
-
 
 
 
